09d89f0553bb71a25a92971a63a18da36ac79837
Deep Patel <dp3676991@gmail.com>
2023-03-07 10:26:39 +0530

    Update on HOd Home Page, Sidebar, Login, Logout

--- student_management_system/views.py
+++ student_management_system/views.py
@@ -1,5 +1,43 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, HttpResponse
+from app.EmailBackEnd import EmailBackEnd
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 
 def BASE(request):
     return render(request, 'base.html')
+
+
+def LOGIN(request):
+    return render(request, 'login.html')
+
+
+def doLogin(request):
+    if request.method == "POST":
+        user = EmailBackEnd.authenticate(request,
+                                         username=request.POST.get('email'),
+                                         password=request.POST.get('password'),)
+        if user != None:
+            login(request, user)
+            user_type = user.user_type
+            if user_type == '1':
+                return redirect('hod_home')
+            elif user_type == '2':
+                return HttpResponse('This is Staff Panel')
+            elif user_type == '3':
+                return HttpResponse('This is Student Panel')
+            else:
+                # Message
+                messages.error(request, 'Email and Password are Invalid!')
+                return redirect('login')
+        else:
+            # Message
+            messages.error(request, 'Email and Password are Invalid!')
+            return redirect('login')
+    return None
+
+
+def doLogout(request):
+    logout(request)
+    return redirect('login')
